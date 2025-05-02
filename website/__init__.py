929ab80718545ebae84cd4897be3636b4552d6ae
Jiaxun Li <165205340+Jessie8086@users.noreply.github.com>
2025-05-01 23:53:50 -0400

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,7 @@ def create_app():
 
     with app.app_context():
          db.create_all()
+         add_indexes()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -37,3 +38,16 @@ def create_database(app):
         if not path.exists('website/' + DB_NAME):
             db.create_all(app=app)
             print('Created Database!')
+
+def add_indexes():
+    from sqlalchemy import text
+
+    index_sqls = [
+        "CREATE INDEX IF NOT EXISTS idx_note_user_id ON note(user_id);",
+        "CREATE INDEX IF NOT EXISTS idx_note_user_date ON note(user_id, date);",
+        "CREATE INDEX IF NOT EXISTS idx_note_tags_note_id ON note_tags(note_id);",
+        "CREATE INDEX IF NOT EXISTS idx_note_tags_tag_id ON note_tags(tag_id);"
+    ]
+    for sql in index_sqls:
+        db.session.execute(text(sql))
+    db.session.commit()
